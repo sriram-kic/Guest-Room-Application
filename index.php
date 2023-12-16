9083a42134c5b1ced5fc589a655d096d8b6f91cc
John Doe <ramsribca@gmail.com>
2023-12-16 09:56:19 +0530

    image update

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ height: 100%;
         <div class="container-fluid h-custom">
           <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-md-9 col-lg-6 col-xl-5">
-              <img src="./Security On.gif"
+              <img src="./image.gif"
                 class="img-fluid" alt="Sample image">
             </div>
             <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
